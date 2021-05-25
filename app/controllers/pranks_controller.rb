433645d82d7cca477f1e65fe05cec2314edc9123
Massim Shah <massimshah@hotmail.com>
2021-05-25 15:51:33 +0900

    fixed errors

--- app/controllers/pranks_controller.rb
+++ app/controllers/pranks_controller.rb
@@ -1,31 +1,32 @@
 class PranksController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
   def index
     @pranks = policy_scope(Prank)
   end
 
   def show
     @prank = Prank.find(params[:id])
-    authorize @prank 
+    authorize @prank
   end
 
   def new
     @prank = Prank.new
     @prank.user = current_user
-    authorize @prank 
+    authorize @prank
   end
 
   def create
     @prank = Prank.create(prank_params)
     @prank.user = current_user
-    authorize @prank 
+    authorize @prank
     if @prank.save
       redirect_to prank_path(@prank)
     else
-      render :new #show them the create prank view
+      render :new
     end
   end
 
   def prank_params
-    params.require(:prank).permit(:name, :content, :price)
+    params.require(:pranks).permit(:name, :content, :price)
   end
 end
