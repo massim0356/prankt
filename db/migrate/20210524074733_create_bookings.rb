543cfdaaced4a2a96d727a65b27aef809ad3b6dc
Massim Shah <massimshah@hotmail.com>
2021-05-28 15:46:28 +0900

    fixed migrations

--- db/migrate/20210524074733_create_bookings.rb
+++ db/migrate/20210524074733_create_bookings.rb
@@ -2,7 +2,7 @@ class CreateBookings < ActiveRecord::Migration[6.0]
   def change
     create_table :bookings do |t|
       t.date :date
-      t.string :status, :default => 'pending'
+      t.string :status
       t.text :description
       t.references :user, null: false, foreign_key: true
       t.references :prank, null: false, foreign_key: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_27_021010) do
+ActiveRecord::Schema.define(version: 2021_05_28_064111) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,9 +38,8 @@ ActiveRecord::Schema.define(version: 2021_05_27_021010) do
 
   create_table "bookings", force: :cascade do |t|
     t.date "date"
-    t.string "status"
+    t.string "status", default: "pending"
     t.text "description"
-    t.time "time"
     t.bigint "user_id", null: false
     t.bigint "prank_id", null: false
     t.datetime "created_at", precision: 6, null: false
